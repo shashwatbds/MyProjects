7f4e3ecfbd6a27abc746a792b64018165c19f443
shashwatbds <93823045+shashwatbds@users.noreply.github.com>
2022-01-27 18:38:27 +0000

    New Commits

--- Python/Coding/Medium/FindLongestSubstringWithUniqueCharsUni.py
+++ Python/Coding/Medium/FindLongestSubstringWithUniqueCharsUni.py
@@ -12,43 +12,25 @@ class Solution:
     # of the longest substring found in a string paremeter
     # without repeating characters
      
-    # This functionr returns true if all
+    # This function returns true if all
     # characters in s (from index i to j) are
     # distinct, otherwise returns false
-    def areDistinct(self, s, i, j):
-     
-        visited = [0] * (26) #Boolean array of length 26 - length same as number of unique alphabets in English language.
-        # Above array will be used to track duplicacy of an alphabet in a single run of function areDistinct
-     
-        for k in range(i, j + 1):
-            if (visited[ord(s[k]) - ord('a')] == True):
-                return False
-                 
-            visited[ord(s[k]) - ord('a')] = True
-     
-        return True
-     
-    # Returns length of the longest substring
-    # with all distinct characters.
     def lengthOfLongestSubstring(self, s):
-         
-        n = len(s) # length of passed string parameter
-         
-        # Result
-        res = 0
-         
-        for i in range(n):
-            for j in range(i, n):
-                if (self.areDistinct(s, i, j)):
-                    res = max(res, j - i + 1) # could be 0 for first iteration.
-                     
-        return res
-        
+        lsub = 0
+        for i in range(len(s)):
+            curr = ""
+            for j in range(i, len(s)):
+                if s[j] not in curr:
+                    curr += s[j]
+                    lsub = max(lsub, len(curr))
+                else:
+                    break
+        return lsub    
         
         
 solObj= Solution()
 print(solObj.lengthOfLongestSubstring("abcabcbb"))
 print(solObj.lengthOfLongestSubstring("pwwkew"))
-print(solObj.lengthOfLongestSubstring(""))
+print(solObj.lengthOfLongestSubstring("aabaab!bb"))
 
 
