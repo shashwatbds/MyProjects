e726071c14513a4ad7861daa8eab5ca7e630bc15
shashwatbds <93823045+shashwatbds@users.noreply.github.com>
2022-08-16 12:00:38 +0100

    Practice

--- Java/CodingQuestions/src/easy/array/RemoveDuplicateElementsFromSortedArray.java
+++ Java/CodingQuestions/src/easy/array/RemoveDuplicateElementsFromSortedArray.java
@@ -5,20 +5,22 @@ import java.util.Arrays;
 public class RemoveDuplicateElementsFromSortedArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int[] arr = {2,2,2,2,2};
 		Arrays.sort(arr);
 		System.out.println(removeDuplicates(arr));
+		
 		int[] arr1 = {1,2,2,2,2};
 		Arrays.sort(arr1);
 		System.out.println(removeDuplicates(arr1));
+		
 		int[] arr2 = {1,2,3,4,5,6,3};
 		Arrays.sort(arr2);
 		System.out.println(removeDuplicates(arr2));
 	}
 
 	private static int removeDuplicates(int[] inputArray) {
+		
 		int count=0;
 		for(int i=0; i<inputArray.length-1; i++) {
 			if(inputArray[i] == inputArray[i+1]) {
@@ -30,6 +32,7 @@ public class RemoveDuplicateElementsFromSortedArray {
 	}
 	
 	private static void printArr(int[] arr) {
+		
 		for(int i : arr) {
 			System.out.println(i);
 		}
