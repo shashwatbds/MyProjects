ef39bae7af87bb2b63bfe7d2aa4bc72b3ec332d3
shashwatbds <93823045+shashwatbds@users.noreply.github.com>
2022-08-31 13:09:43 +0100

    Practice

---
+++ Java/ImportantConcepts/src/com/java/operators/Operators.java
@@ -0,0 +1,14 @@
+package com.java.operators;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		System.out.println(++i);
+		System.out.println(--i);
+		System.out.println(++i + --i);
+
+	}
+
+}
