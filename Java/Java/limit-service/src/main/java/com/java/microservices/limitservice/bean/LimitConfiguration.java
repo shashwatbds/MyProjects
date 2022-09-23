5e66b6f7d195c2167f3aa78b919cb64d3f35dfa4
shashwatbds <93823045+shashwatbds@users.noreply.github.com>
2022-09-23 18:48:10 +0100

    Microservice Practice

---
+++ Java/Java/limit-service/src/main/java/com/java/microservices/limitservice/bean/LimitConfiguration.java
@@ -0,0 +1,32 @@
+package com.java.microservices.limitservice.bean;
+
+public class LimitConfiguration {
+
+	private int maximum, minimum;
+	
+	protected LimitConfiguration() {}
+
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	};
+	
+	
+}
