bf018633688c6a019df716443164c1a598a7380c
shashwat bhardwaj <shashwatbds@gmail.com>
2024-07-08 13:40:37 +0100

    Palindrome

---
+++ Java/LeetCode/src/main/java/org/example/easy/PalimromeCheck.java
@@ -0,0 +1,52 @@
+package org.example.easy;
+
+
+public class PalimromeCheck {
+
+    public static void main(String[] args) {
+        PalimromeCheck palimromeCheck = new PalimromeCheck();
+        System.out.println("121 - " + palimromeCheck.isPalindrome(121));
+        System.out.println("1210 - " + palimromeCheck.isPalindrome(1210));
+        System.out.println("-121 - " + palimromeCheck.isPalindrome(-121));
+        System.out.println("10 - " + palimromeCheck.isPalindrome(10));
+        System.out.println("1001 - " + palimromeCheck.isPalindrome(1001));
+
+        System.out.println("\n\nEfficient Way to follow now - \n\n");
+
+        System.out.println("121 - " + palimromeCheck.isPalindromeEfficient(121));
+        System.out.println("1210 - " + palimromeCheck.isPalindromeEfficient(1210));
+        System.out.println("-121 - " + palimromeCheck.isPalindromeEfficient(-121));
+        System.out.println("10 - " + palimromeCheck.isPalindromeEfficient(10));
+        System.out.println("1001 - " + palimromeCheck.isPalindromeEfficient(1001));
+    }
+
+    private boolean isPalindrome(int x) {
+
+        int z = x;
+        int reversed = 0;
+        while(x>0) {
+            int lastDigit = x%10;//1,0,0,1
+            reversed = reversed*10+lastDigit;//1,10,100,1001
+            x/=10;//100,10,1,0
+        }
+        return reversed==z;
+    }
+
+    private boolean isPalindromeEfficient(int x) {
+
+        if(x<0 //negetive number can't be a palindrome
+                || (x!=0 && x%10==0)) { //if last digit of the number is 0 then then number isn't a palindrome since number can not begin with zero.
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+        while(x>reversed) {
+            int lastDigit = x%10;
+            reversed = reversed*10+lastDigit;
+            x/=10;
+        }
+        return (reversed==x) //if number has even number of digits
+                || (x==reversed/10); //if number has odd number of digits
+    }
+}
