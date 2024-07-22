a8b9f51ce46966a57216eb3140c9e5c605048818
shashwat bhardwaj <shashwatbds@gmail.com>
2024-07-22 21:26:02 +0100

    Leet Code blind 75

---
+++ Java/LeetCode/src/main/java/org/example/blind75/medium/MinimumInSortedRotatedArray.java
@@ -0,0 +1,43 @@
+package org.example.blind75.medium;
+
+public class MinimumInSortedRotatedArray {
+
+    //Solve in O(logn) - as soon as we get log(n) complexity we know this will be solved by binary search.
+    //In each iteration we will discard half of the elements from consideration hence the time complexity is logn
+
+    public static void main(String[] args) {
+        MinimumInSortedRotatedArray obj = new MinimumInSortedRotatedArray();
+        System.out.println(obj.findMin(new int[]{3,4,5,1,2}));
+        System.out.println(obj.findMin(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(obj.findMin(new int[]{11,13,15,17}));
+        System.out.println(obj.findMin(new int[]{2,1}));
+    }
+
+    /**
+     * We will maintain 2 pointers and compare both to find if left pointer is less than right pointer.
+     *  - if l < r, we can assume range l and r is sorted and l is minimum in the range.
+     *  - if l > r then minimum lies between l and r but not at l or r.
+     *      - We find middle value and if the middle value is > r then the minimum lies between middle and r else it lies between l and middle.
+     * */
+    public int findMin(int[] nums) {
+
+        int ans = nums[0], left = 0, right = nums.length-1;
+        if(nums.length == 1) {
+            return nums[0];
+        }
+
+        while(left <= right) {
+            if(nums[left] < nums[right]) {
+                ans = Math.min(ans, nums[left]);
+            }
+            int mid = (left + right) / 2;
+            ans = Math.min(ans, nums[mid]);
+            if(nums[left] <= nums[mid]) {
+                left = mid +1;
+            } else {
+                right = mid -1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ Java/LeetCode/src/main/java/org/example/blind75/medium/SearchInARotatedSortedArray.java
@@ -0,0 +1,37 @@
+package org.example.blind75.medium;
+
+public class SearchInARotatedSortedArray {
+
+    public static void main(String[] args) {
+        SearchInARotatedSortedArray obj = new SearchInARotatedSortedArray();
+        System.out.println(obj.search(new int[]{4,5,6,7,0,1,2}, 0));
+        System.out.println(obj.search(new int[]{4,5,6,7,0,1,2}, 3));
+        System.out.println(obj.search(new int[]{1}, 0));
+    }
+
+
+    public int search(int[] nums, int target) {
+        int ans = nums[0], left = 0, right = nums.length-1;
+
+        while(left <= right) {
+            int mid = (left + right)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[left] <= nums[mid]) {
+                if(target < nums[left] || target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid -1;
+                }
+            } else {
+                if(target > nums[right] || target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
