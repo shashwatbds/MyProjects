6371ccfe716394329ed0a3fd7e39352588db6f06
shashwat bhardwaj <shashwatbds@gmail.com>
2024-07-25 21:58:41 +0100

    Leet Code blind 75

---
+++ Java/LeetCode/src/main/java/org/example/blind75/easy/ClimbingSteps.java
@@ -0,0 +1,33 @@
+package org.example.blind75.easy;
+
+/**
+ * This is classic dynamic programming problem.
+ * For each step we will save the number of routes possible at each stair and we can avoid recalculation everytime.
+ * In this manner we will be able to have time complexity of O(n)
+ * Brute force method will be recursion. Time complexity will be 2^n since for each node we will have two different options, 1 step or 2 steps.
+ * */
+public class ClimbingSteps {
+
+    public static void main(String[] args) {
+
+        ClimbingSteps obj = new ClimbingSteps();
+        System.out.println(obj.climbStairs(2));
+        System.out.println(obj.climbStairs(3));
+        System.out.println(obj.climbStairs(4));
+    }
+
+    public int climbStairs(int n) {
+
+        if(n == 1) {
+            return 1;
+        }
+        int one = 1, two = 2;
+        for(int i = 3; i<=n; i++) {
+
+            int total = one + two;
+            one = two;
+            two = total;
+        }
+        return two;
+    }
+}
