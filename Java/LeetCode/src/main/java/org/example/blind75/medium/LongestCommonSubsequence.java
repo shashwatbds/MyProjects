4c620e7d5c91d68029dc26476b394f3131874b95
shashwat bhardwaj <shashwatbds@gmail.com>
2024-08-01 16:31:28 +0100

    Leet Code blind 75

--- Java/LeetCode/src/main/java/org/example/blind75/medium/LongestCommonSubsequence.java
+++ Java/LeetCode/src/main/java/org/example/blind75/medium/LongestCommonSubsequence.java
@@ -8,15 +8,54 @@ public class LongestCommonSubsequence {
 
     public static void main(String[] args) {
         LongestCommonSubsequence obj = new LongestCommonSubsequence();
-        System.out.println(obj.longestCommonSubsequence("abcde","ace"));
-        System.out.println(obj.longestCommonSubsequence("abc","abc"));
-        System.out.println(obj.longestCommonSubsequence("abc","def"));
+        System.out.println(obj.longestCommonSubsequenceBF("abcde","ace"));
+        System.out.println(obj.longestCommonSubsequenceBF("abc","abc"));
+        System.out.println(obj.longestCommonSubsequenceBF("abc","def"));
+
+        System.out.println("\n\n");
+
+        System.out.println(obj.longestCommonSubsequenceMemoization("abcde","ace"));
+        System.out.println(obj.longestCommonSubsequenceMemoization("abc","abc"));
+        System.out.println(obj.longestCommonSubsequenceMemoization("abc","def"));
+
     }
 
-    public int longestCommonSubsequence(String text1, String text2) {
+    /**
+     * https://www.youtube.com/watch?v=PieZjz2Pyhw&t=8879s
+     * */
+    public int longestCommonSubsequenceMemoization(String text1, String text2) {
 
-        int max = 0;
+        int[][] matrix = new int[text1.length()+1][text2.length()+1];//We have set size of array as 1 more than length to avoid null pointer exception.
 
-        return max;
+        for(int j = text2.length()-1; j>=0; j--) {
+            for(int i = text1.length()-1; i>=0; i--) {
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    matrix[i][j] = 1 + matrix[i+1][j+1];//1 more than the diagonal
+                } else {
+                    matrix[i][j] = Math.max(matrix[i+1][j], matrix[i][j+1]);
+                }
+            }
+        }
+        return matrix[0][0];
+    }
+
+    /**
+     * Brute force approach - In this case we won't use any memoization.
+     * This approach has limitation that for longer strings java programs could go on running for a long duration and take up lot of space.
+     * Time complexity - O(2^n) - at every single location we have two options.
+     * */
+    public int longestCommonSubsequenceBF(String text1, String text2) {
+        return lcsRecursiveBF(text1, text2, text1.length(), text2.length());//Index for tracking chars on text1 and text2 respectively.
+    }
+
+    private int lcsRecursiveBF(String text1, String text2, int n, int m) {
+        //Base case: stops recursion.
+        if (n == 0 || m == 0)//This means we have reached end of one of the strings and can not further have a longer substring sequence.
+            return 0;
+
+        if (text1.charAt(n - 1) == text2.charAt(m - 1))//if char is same the index
+            return 1 + lcsRecursiveBF(text1, text2, n - 1, m - 1);
+
+        return Math.max(lcsRecursiveBF(text1, text2, n - 1, m), lcsRecursiveBF(text1, text2, n, m - 1));
     }
 }
---
+++ Java/LeetCode/src/main/java/org/example/blind75/medium/WordBreak.java
@@ -0,0 +1,32 @@
+package org.example.blind75.medium;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    public static void main(String[] args) {
+        WordBreak obj = new WordBreak();
+        System.out.println(obj.wordBreak("leetcode", Arrays.asList("leet","code")));
+        System.out.println(obj.wordBreak("applepenapple", Arrays.asList("apple","pen")));
+        System.out.println(obj.wordBreak("catsandog", Arrays.asList("cats","dog","sand","and","cat")));
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        for(int i=1; i<=s.length(); i++) {
+            for(int j=i-1; j>=0; j--) {
+                if(dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
