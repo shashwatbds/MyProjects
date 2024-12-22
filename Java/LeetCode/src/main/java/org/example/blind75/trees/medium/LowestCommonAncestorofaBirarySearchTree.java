6d065563357015471a787de33d6851b4f74a6af8
shashwat bhardwaj <shashwatbds@gmail.com>
2024-12-22 22:12:32 +0000

    Modifying Logic

--- Java/LeetCode/src/main/java/org/example/blind75/trees/medium/LowestCommonAncestorofaBirarySearchTree.java
+++ Java/LeetCode/src/main/java/org/example/blind75/trees/medium/LowestCommonAncestorofaBirarySearchTree.java
@@ -6,14 +6,20 @@ public class LowestCommonAncestorofaBirarySearchTree {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        int parentVal = root.val;
-        int pVal = p.val;
-        int qVal = q.val;
-        if(pVal > parentVal && qVal > parentVal)//means value of both p & q are greater than value of root and have to exist in right subtree of root
-            return lowestCommonAncestor(root.right, p, q);
-        if(pVal < parentVal && qVal < parentVal)//means value of both p & q are lower than value of root and have to exist in left subtree of root
-            return lowestCommonAncestor(root.left, p, q);
-        else //means one is lower and one is greater hence only root could be lowest ancestor
-            return root;
+        // Base case: null node
+        if (root == null) return null;
+
+        // If the current node is either p or q, return it
+        if (root == p || root == q) return root;
+
+        // Recur for left and right children
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // If both left and right return a non-null value, current node is LCA
+        if (left != null && right != null) return root;
+
+        // Otherwise, return the non-null child (or null if both are null)
+        return left != null ? left : right;
     }
 }
