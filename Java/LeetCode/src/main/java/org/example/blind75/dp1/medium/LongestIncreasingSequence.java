9397609baa49c675714b5a411dd51b5a15066d06
shashwat bhardwaj <shashwatbds@gmail.com>
2025-01-12 17:23:56 +0000

    Modifying Logic

--- Java/LeetCode/src/main/java/org/example/blind75/dp1/medium/LongestIncreasingSequence.java
+++ Java/LeetCode/src/main/java/org/example/blind75/dp1/medium/LongestIncreasingSequence.java
@@ -1,6 +1,9 @@
 package org.example.blind75.dp1.medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Dynamic Programming
@@ -9,9 +12,13 @@ public class LongestIncreasingSequence {
 
     public static void main(String[] args) {
         LongestIncreasingSequence obj = new LongestIncreasingSequence();
-        System.out.println(obj.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
-        System.out.println(obj.lengthOfLIS(new int[]{0,1,0,3,2,3}));
-        System.out.println(obj.lengthOfLIS(new int[]{7,7,7,7,7,7,7}));
+        System.out.println(4 == obj.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(4 == obj.lengthOfLIS(new int[]{0,1,0,3,2,3}));
+        System.out.println(1 == obj.lengthOfLIS(new int[]{7,7,7,7,7,7,7}));
+
+        System.out.println(4 == obj.lengthOfLISBinarySearch(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(4 == obj.lengthOfLISBinarySearch(new int[]{0,1,0,3,2,3}));
+        System.out.println(1 == obj.lengthOfLISBinarySearch(new int[]{7,7,7,7,7,7,7}));
     }
 
     /**
@@ -23,10 +30,10 @@ public class LongestIncreasingSequence {
         Arrays.fill(lis, 1);
         int max = 1;
 
-        for(int i=1; i< nums.length; i++) {
+        for(int i=1; i< nums.length; i++) {//start with 2nd element so that we can compare it with first element. value at first element will always be 1.
             for(int j=0; j<i; j++) {
                 if(nums[i] > nums[j]) {
-                    lis[i] = Math.max(lis[i], 1+lis[j]);
+                    lis[i] = Math.max(lis[i], 1+lis[j]);//recurrence relation = Interview Term to be used.
                     max = Math.max(lis[i], max);
                 }
             }
@@ -36,42 +43,38 @@ public class LongestIncreasingSequence {
 
 
     /**
-     * Time complexity is O(n)*O(n-1) = O(n^2) - because for each number we will compare the number with all its previous numbers.
+     * Time complexity is O(nlogn) - because for will iterate the array once so n and for binary search it would be nlogn so total complexity is n+ nlogn which is similar to O(nlogn)
      * */
-    public int lengthOfLISRecursion(int[] nums) {
+    public int lengthOfLISBinarySearch(int[] nums) {
 
-        int[] lis = new int[nums.length];
-        Arrays.fill(lis, 1);
-        int max = 1;
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
 
-        for(int i=1; i< nums.length; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    lis[i] = Math.max(lis[i], 1+lis[j]);
-                    max = Math.max(lis[i], max);
+        // Initialize the answer list with the first element of nums
+        ans.add(nums[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > ans.get(ans.size() - 1)) {
+                // If the current number is greater than the last element of the answer
+                // list, it means we have found a longer increasing subsequence.
+                // Hence, we append the current number to the answer list.
+                ans.add(nums[i]);
+            } else {
+                // If the current number is not greater than the last element of the answer list, we perform
+                // a binary search to find the smallest element in the answer list that is greater than or equal to the
+                // current number.
+                // The binarySearch method returns the index of the first element that is not less than the current number.
+                int low = Collections.binarySearch(ans, nums[i]);
+
+                // We update the element at the found position with the current number.
+                // By doing this, we are maintaining a sorted order in the answer list.
+                if (low < 0) {
+                    low = -(low + 1);
                 }
+                ans.set(low, nums[i]);
             }
         }
-        return max;
-    }
-
-    /**
-     * Time complexity is O(n)*O(n-1) = O(n^2) - because for each number we will compare the number with all its previous numbers.
-     * */
-    public int lengthOfLISRecursionMemoization(int[] nums) {
-
-        int[] lis = new int[nums.length];
-        Arrays.fill(lis, 1);
-        int max = 1;
-
-        for(int i=1; i< nums.length; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    lis[i] = Math.max(lis[i], 1+lis[j]);
-                    max = Math.max(lis[i], max);
-                }
-            }
-        }
-        return max;
+        // The size of the answer list represents the length of the longest increasing subsequence.
+        return ans.size();
     }
 }
